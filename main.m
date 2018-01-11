5c23df1dce3b8553995256f77fb7fb7352372729
kitchell <lindseykitchell21@gmail.com>
2018-01-10 21:37:50 -0500

    update

--- main.m
+++ main.m
@@ -24,8 +24,7 @@ for file = 1:size(filelist)
     [V,F] = read_vtk([config.surfaces '/' filelist(file).name]);
     %size(V)
     %size(F)
-% evecs.(filelist(file).name(1:end-4))
-    iminfo = plot_eigenfunction(V, F, evecs, evnum, filelist(file).name(1:end-4));
+    iminfo = plot_eigenfunction(V, F, evecs.(filelist(file).name(1:end-4)), evnum, filelist(file).name(1:end-4));
     for im = 1:size(iminfo, 2)
         json.images(((file-1) * 6) + im) = iminfo(im);
     end
